7b8694d9f3a17e41a2c64728ff4beb65343c5235
PatrykW7 <patryk90020@o2.pl>
2022-11-09 21:54:49 +0100

    bodyDetectorChanges

--- bodyDetectionModule.py
+++ bodyDetectionModule.py
@@ -18,6 +18,7 @@ class bodyDetection():
         self.results=self.holistic.process(imageRGB)
         if self.results.pose_landmarks:
             if draw:
+                
                 #kolorowanie - nie konieczne 
                 self.mpDraw.draw_landmarks(img,self.results.pose_landmarks,self.mpHolistic.POSE_CONNECTIONS,self.mpDraw.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2))
                 
@@ -28,7 +29,7 @@ class bodyDetection():
         cap.set(3,1920)
         cap.set(4,1080)
         return cap
-
+    '''
     def change_res(self,cap,width,height):
         cap.set(3,width)
         cap.set(4,height)
@@ -39,14 +40,19 @@ class bodyDetection():
         height=int(img.shape[0]*scale_percent/100)
         dim=(width/height)
         return cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
-
+    '''
     def getPosition(self,img):
         lmList=[]
         if self.results.pose_landmarks:
+            
+            
             for id,lm in enumerate(self.results.pose_landmarks.landmark):
                 h,w,c=img.shape
                 cx,cy=int(lm.x*w),int(lm.y*h)
                 lmList.append([id,cx,cy])
+                #print(lmList[id][0])
+            #wypisywanie wspolrzednych nosa
+            print(self.results.pose_landmarks.landmark[self.mpHolistic.PoseLandmark.NOSE].x * w)
         return lmList
                 
 def main():
@@ -54,22 +60,28 @@ def main():
     cap=cv2.VideoCapture(0)
     detector=bodyDetection()
     cap=detector.make_1080(cap) 
+    ptime=0
     while True:
         success,img=cap.read()   
            
         img=detector.findBody(img)
         lmList=detector.getPosition(img)
-        print(lmList)
-        #print(img.shape)
         
+        #print(lmList[15])
+        #print(img.shape)
+        ctime=time.time()
+        fps=1/(ctime-ptime)
+        ptime=ctime
         #img=detector.rescale_frame(img)
         #print(img.shape)
+        cv2.putText(img,str(int(fps)),(70,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
         cv2.imshow("Image",img)
+        
         if cv2.waitKey(1)==ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows
-
+    #
 
 if __name__=="__main__":
     main()
